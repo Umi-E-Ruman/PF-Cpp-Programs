48ecfaeb35b650046cb20bb564ff29197b1c8f84
Umi-E-Ruman <ummeroman488@gmail.com>
2025-08-06 23:01:57 +0500

    Added 3 decision-based programs with comments: Speed Category Detector, Challan Warning, and Playtime Calculator

---
+++ challan_warning.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int speed;
+cout<<"Enter your speed";
+cin>>speed;
+if(speed>100)
+cout<<"Halt...you will be CHALLENED";
+else
+cout<<"Perfect! you're going good";
+
+
+}
---
+++ play_time_calculator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int noOfHoliday ;
+int noOfWorkDays ;
+int totalNorm;
+int noOfSeconds;
+int convertValue=0;
+
+ cout<<" Enter no of holidays ";
+ cin>>noOfHoliday;
+ noOfWorkDays=365-noOfHoliday;
+noOfSeconds=noOfHoliday*127+noOfWorkDays*63;
+
+
+
+
+
+cout<<"time left for play  : "<<totalNorm;
+
+return 0;
+}
